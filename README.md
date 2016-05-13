89a896f386bbc2719b9d710e2863dbfd559fa9eb
henricasanova <henric@hawaii.edu>
2016-05-12 15:41:48 -1000

    removed non-generated content from gh-pages branch

--- README.md
+++
@@ -1,1 +0,0 @@
-# SMPI_CourseWare
--- generate_html.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-( set -x ; /bin/rm -rf ./gh-pages )
-( set -x ; mkdir ./gh-pages )
-( set -x ; cd ./gh-pages; git clone -b gh-pages https://github.com/simgrid/SMPI_CourseWare.git ; )
-( set -x ; jekyll build --source ./site --destination ./gh-pages)
-( set -x; cd gh-pages; git pull; git add --all . ; git commit -a -m "updated content"; git push origin gh-pages)
-( set -x; /bin/rm -rf ./gh-pages )
