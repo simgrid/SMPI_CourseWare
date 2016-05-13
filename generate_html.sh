cd94337c37a0352d278154643e02f4dc144f7791
henricasanova <henric@hawaii.edu>
2016-05-12 15:46:35 -1000

    Debugged kekyll script removed generated content from master branch

--- generate_html.sh
+++ generate_html.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 
 ( set -x ; /bin/rm -rf ./gh-pages )
-( set -x ; mkdir ./gh-pages )
-( set -x ; cd ./gh-pages; git clone -b gh-pages https://github.com/simgrid/SMPI_CourseWare.git ; )
+( set -x ; git clone https://github.com/simgrid/SMPI_CourseWare.git gh-pages; cd gh-pages; git checkout gh-pages; git branch -u origin/gh-pages; cd ..)
 ( set -x ; jekyll build --source ./site --destination ./gh-pages)
-( set -x; cd ./gh-pages; git pull; git add --all . ; git commit -a -m "updated content"; git push origin gh-pages)
+( set -x; cd gh-pages; git add --all .; git commit -a; git push; cd ..)
 ( set -x; /bin/rm -rf ./gh-pages )
