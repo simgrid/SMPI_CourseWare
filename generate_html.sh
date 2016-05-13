550a7f8eaf354b84594ba9d73131dff857e7cdcc
henricasanova <henric@hawaii.edu>
2016-05-12 15:28:42 -1000

    updated content

--- generate_html.sh
+++ generate_html.sh
@@ -4,5 +4,5 @@
 ( set -x ; mkdir ./gh-pages )
 ( set -x ; cd ./gh-pages; git clone -b gh-pages https://github.com/simgrid/SMPI_CourseWare.git ; )
 ( set -x ; jekyll build --source ./site --destination ./gh-pages)
-( set -x; cd gh-pages; git pull; git add --all . ; git commit -a -m "updated content"; git push origin gh-pages)
+( set -x; cd ./gh-pages; git pull; git add --all . ; git commit -a -m "updated content"; git push origin gh-pages)
 ( set -x; /bin/rm -rf ./gh-pages )
