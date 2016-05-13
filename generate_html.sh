50ee1f5dbd6f5f84e81608c2b9c8a6147c9367cb
henricasanova <henric@hawaii.edu>
2016-05-12 16:05:24 -1000

    Updated README Added somme comments to generate_html.sh

--- generate_html.sh
+++ generate_html.sh
@@ -1,4 +1,11 @@
 #!/bin/bash
+#
+# This script should be executed on a checkout of the master branch and it:
+#   - creates a gh-pages directory
+#   - does a check out of the gh-pages branch in that directory
+#   - runs jekyll to generate content into the gh-pages directory
+#   - pushes the changes to the gh-pages branch
+#   - the gh-branch content is magically seen at: https://simgrid.github.io/SMPI_CourseWare/
 
 ( set -x ; /bin/rm -rf ./gh-pages )
 ( set -x ; git clone https://github.com/simgrid/SMPI_CourseWare.git gh-pages; cd gh-pages; git checkout gh-pages; git branch -u origin/gh-pages; cd ..)
