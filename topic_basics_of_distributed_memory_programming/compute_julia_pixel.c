5bcd59574f933fabf6bdf91d786d41bd6072888c
henricasanova <henric@hawaii.edu>
2016-05-20 21:50:52 -1000

    Re-generating HTML

---
+++ topic_basics_of_distributed_memory_programming/compute_julia_pixel.c
@@ -0,0 +1,56 @@
+
+/*
+ * compute_julia_pixel(): compute RBG values of a pixel in a
+ *                        particular Julia set image.
+ *
+ *  In:
+ *      (x,y):            pixel coordinates
+ *      (width, height):  image dimensions
+ *      tint_bias:        a float to "tweak" the tint (1.0 is "no tint")
+ *  Out:
+ *      rgb: an already-allocated 3-byte array into which R, G, and B
+ *           values are written.
+ *
+ *  Return:
+ *      0 in success, -1 on failure
+ *
+ */
+
+int compute_julia_pixel(int x, int y, int width, int height, float tint_bias, unsigned char *rgb) {
+
+  // Check coordinates
+  if ((x < 0) || (x >= width) || (y < 0) || (y >= height)) {
+    fprintf(stderr,"Invalid (%d,%d) pixel coordinates in a %d x %d image\n", x, y, width, height);
+    return -1;
+  }
+
+  // "Zoom in" to a pleasing view of the Julia set
+  float X_MIN = -1.6, X_MAX = 1.6, Y_MIN = -0.9, Y_MAX = +0.9;
+  float float_y = (Y_MAX - Y_MIN) * (float)y / height + Y_MIN ;
+  float float_x = (X_MAX - X_MIN) * (float)x / width  + X_MIN ;
+
+  // Point that defines the Julia set
+  float julia_real = -.79;
+  float julia_img = .15;
+
+  // Maximum number of iteration
+  int max_iter = 300;
+
+  // Compute the complex series convergence
+  float real=float_y, img=float_x;
+  int num_iter = max_iter;
+  while (( img * img + real * real < 2 * 2 ) && ( num_iter > 0 )) {
+    float xtemp = img * img - real * real + julia_real;
+    real = 2 * img * real + julia_img;
+    img = xtemp;
+    num_iter--;
+  }
+
+  // Paint pixel based on how many iterations were used, using some funky colors
+  float color_bias = (float) num_iter / max_iter;
+  rgb[0] = (num_iter == 0 ? 200 : - 500.0 * pow(tint_bias, 1.2) *  pow(color_bias, 1.6));
+  rgb[1] = (num_iter == 0 ? 100 : -255.0 *  pow(color_bias, 0.3));
+  rgb[2] = (num_iter == 0 ? 100 : 255 - 255.0 * pow(tint_bias, 1.2) * pow(color_bias, 3.0));
+
+  return 0;
+}
