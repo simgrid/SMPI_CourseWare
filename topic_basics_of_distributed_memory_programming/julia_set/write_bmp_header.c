75b149c5343446e01b2734cb118cc4651694d307
henricasanova <henric@hawaii.edu>
2016-05-22 16:43:06 -1000

    Re-generating HTML

---
+++ topic_basics_of_distributed_memory_programming/julia_set/write_bmp_header.c
@@ -0,0 +1,50 @@
+/* write_bmp_header():
+ *
+ *   In:
+ *      f: A file open for writing ('w') 
+ *      (width, height): image dimensions
+ *   
+ *   Return:
+ *      0 on success, -1 on failure
+ *
+ */
+
+int write_bmp_header(FILE *f, int width, int height) {
+
+  // Define all fields in the bmp header
+  char id[2] = "BM";
+  unsigned int filesize = 102 + (int)(width * height * 3 * sizeof(char));
+  short reserved[2] = {0,0};
+  unsigned int offset = 54;
+  unsigned int size = 40;
+  unsigned short planes = 1;
+  unsigned short bits = 24;
+  unsigned int compression = 0;
+  unsigned int image_size = width * height * 3 * sizeof(char);
+  int x_res = 0;
+  int y_res = 0;
+  unsigned int ncolors = 0;
+  unsigned int importantcolors = 0;
+
+  // Write the bytes to the file, keeping track of the
+  // number of written "objects"
+  size_t ret = 0;
+  ret += fwrite(id, sizeof(char), 2, f);
+  ret += fwrite(&filesize, sizeof(int), 1, f);
+  ret += fwrite(reserved, sizeof(short), 2, f);
+  ret += fwrite(&offset, sizeof(int), 1, f);
+  ret += fwrite(&size, sizeof(int), 1, f);
+  ret += fwrite(&width, sizeof(int), 1, f);
+  ret += fwrite(&height, sizeof(int), 1, f);
+  ret += fwrite(&planes, sizeof(short), 1, f);
+  ret += fwrite(&bits, sizeof(short), 1, f);
+  ret += fwrite(&compression, sizeof(int), 1, f);
+  ret += fwrite(&image_size, sizeof(int), 1, f);
+  ret += fwrite(&x_res, sizeof(int), 1, f);
+  ret += fwrite(&y_res, sizeof(int), 1, f);
+  ret += fwrite(&ncolors, sizeof(int), 1, f);
+  ret += fwrite(&importantcolors, sizeof(int), 1, f);
+
+  // Success means that we wrote 17 "objects" successfully
+  return (ret != 17);
+}
