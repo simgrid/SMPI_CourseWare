37fdb60650feda12155679995c2eb4dbdd258e63
Martin Quinson <martin.quinson@loria.fr>
2016-08-27 17:36:15 +0200

    Re-generating HTML

--- generate_and_publish_html.sh
+++ generate_and_publish_html.sh
@@ -8,7 +8,7 @@
 #   - the gh-branch content is magically seen at: https://simgrid.github.io/SMPI_CourseWare/
 
 ( set -x ; /bin/rm -rf /tmp/gh-pages )
-( set -x ; cd /tmp; git clone https://github.com/simgrid/SMPI_CourseWare.git gh-pages; cd gh-pages; git checkout gh-pages; git branch -u origin/gh-pages; )
+( set -x ; cd /tmp; git clone git@github.com:simgrid/SMPI_CourseWare.git gh-pages; cd gh-pages; git checkout gh-pages; git branch -u origin/gh-pages; )
 ( set -x ; jekyll build --source . --destination /tmp/gh-pages; )
 ( set -x; cd /tmp/gh-pages; git add --all .; git commit -a -m "Re-generating HTML"; git push; )
 ( set -x; /bin/rm -rf /tmp/gh-pages; )
